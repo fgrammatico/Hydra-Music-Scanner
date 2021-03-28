a580421fcb698a887a313f754947fb1a3b8f9d9c
NyanCoder <fabiogrammatico+github@pm.me>
2021-03-28 15:51:04 +0200

    test token

--- get id.py
+++ get id.py
@@ -14,17 +14,8 @@ sp = spotipy.Spotify(auth_manager=auth_manager)
 playlists = sp.user_playlists(SPOTIPY_USER)
 while playlists:
     for i, playlist in enumerate(playlists['items']):
-        #print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name'])) this is a memo of full syntax
-        pl_id = playlist['uri']
-        offset = 0
-        while True:
-            response = sp.playlist_items(pl_id,offset=offset,fields='items.track.id,total',additional_types=['track'])
-            if len(response['items']) == 0:
-                break
-            #
-            pprint(response['items'])
-            offset = offset + len(response['items'])
-            print(offset, "/", response['total'])
+        print(playlist['name'])
+        print(playlist['id'])
     if playlists['next']:
         playlists = sp.next(playlists)
     else:
