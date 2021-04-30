83e5f469a67ead1fa5131b718b2af31c0e068427
NyanCat <fabiogrammatico@protonmail.ch>
2021-04-30 16:25:46 +0200

    edit

---
+++ Scan_Playlists.py
@@ -0,0 +1,40 @@
+import spotipy
+import os
+from spotipy.oauth2 import SpotifyClientCredentials
+from pprint import pprint
+import json
+import boto3
+
+SPOTIPY_CLIENT_ID=os.environ['SPOTIPY_CLIENT_ID']
+SPOTIPY_CLIENT_SECRET=os.environ['SPOTIPY_CLIENT_SECRET']
+SPOTIPY_USER=os.environ['SPOTIPY_USER']
+AWS_ACCESS_KEY_ID=os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY=os.environ['AWS_SECRET_ACCESS_KEY']
+clientDynamo = boto3.client('dynamodb')
+
+auth_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
+def scan_playlists(SPOTIPY_CLIENT_ID,SPOTIPY_CLIENT_SECRET,SPOTIPY_USER):
+    ''' Get Playlists for the user'''
+    playlists = sp.user_playlists(SPOTIPY_USER)
+    while playlists:
+        for i, playlist in enumerate(playlists['items']):
+            playlistName = playlist['name']
+            playlistID = playlist['id']
+            response = clientDynamo.get_item(
+                Key = {
+                    '26MuX4iyxKcYv3kUzqolJo' : {
+                        'S' : '5678',
+                        },
+                        },
+                TableName = 'Thievery.And.Holiday',
+                )
+            print (playlistName)
+
+        if playlists['next']:
+            playlists = sp.next(playlists)
+        else:
+            playlists = None
+
+scan_playlists(SPOTIPY_CLIENT_ID,SPOTIPY_CLIENT_SECRET,SPOTIPY_USER)    
